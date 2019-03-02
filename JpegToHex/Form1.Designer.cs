b6ba302c32ab3e711134920551d86928ce11a2bc
Yury Vostrenkov <yuvostrenkov@gmail.com>
2019-03-02 15:19:44 +0300

    initial commit

---
+++ JpegToHex/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace JpegToHex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.open_button = new System.Windows.Forms.Button();
+            this.export_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // open_button
+            // 
+            this.open_button.Location = new System.Drawing.Point(161, 12);
+            this.open_button.Name = "open_button";
+            this.open_button.Size = new System.Drawing.Size(146, 40);
+            this.open_button.TabIndex = 0;
+            this.open_button.Text = "Open JPEG";
+            this.open_button.UseVisualStyleBackColor = true;
+            this.open_button.Click += new System.EventHandler(this.open_button_Click);
+            // 
+            // export_button
+            // 
+            this.export_button.Location = new System.Drawing.Point(495, 12);
+            this.export_button.Name = "export_button";
+            this.export_button.Size = new System.Drawing.Size(146, 40);
+            this.export_button.TabIndex = 1;
+            this.export_button.Text = "Export Array";
+            this.export_button.UseVisualStyleBackColor = true;
+            this.export_button.Click += new System.EventHandler(this.export_button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(84, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(627, 325);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(84, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(627, 27);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(374, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 452);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.export_button);
+            this.Controls.Add(this.open_button);
+            this.Name = "Form1";
+            this.Text = "JpegToHex";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open_button;
+        private System.Windows.Forms.Button export_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JpegToHex/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JpegToHex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void open_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "JPEG Images| *.jpeg; *.jpg| All Files| *.*";
+            openFileDialog.Title = "Open Image";
+            // openFileDialog.InitialDirectory = @"C:\";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        private void export_button_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+                saveFileDialog.Filter = "Text Files| *.txt| All Files| *.*";
+                saveFileDialog.Title = "Select File to Export";
+                // saveFileDialog.InitialDirectory = @"C:\";
+                saveFileDialog.RestoreDirectory = true;
+                //saveFileDialog.CheckFileExists = true;
+                saveFileDialog.CheckPathExists = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Image img = pictureBox1.Image;
+                    byte[] arr;
+                    string str = "";
+                    string init = "0x";
+                    byte[] byte_str;
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        arr = ms.ToArray();
+
+                        File.WriteAllText(saveFileDialog.FileName, string.Empty); // clear file
+
+                        progressBar1.Maximum = arr.Length;
+                        progressBar1.Value = 0;
+                        label1.Text = "";
+
+                        int j = 0;
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+                            if (i == 0)
+                            {
+                                str ="uint8_t image[" + arr.Length + "] = " + 
+                                    "{ \r\n" + init + arr[i].ToString("X" + 2) + ",";
+                                j+=2;
+                            }
+                            else if (i < arr.Length - 1)
+                            {
+
+                                if (j >= 32)
+                                {
+                                    str += init + arr[i].ToString("X" + 2) + "," + "\r\n";
+                                    j = 0;
+                                }
+                                else
+                                {
+                                    str += init + arr[i].ToString("X" + 2) + ",";
+                                }
+                                j++;
+                            }
+                            else if (i == arr.Length - 1)
+                            {
+                                str += init + arr[i].ToString("X" + 2) + "\r\n};";
+                            }
+
+
+                            progressBar1.Value = i;
+                            Application.DoEvents();
+                        }
+                        byte_str = Encoding.ASCII.GetBytes(str);
+                        using (FileStream fstream = new FileStream(saveFileDialog.FileName, FileMode.Append))
+                        {
+                            fstream.Write(byte_str, 0, str.Length);
+                            fstream.Flush();
+                        }
+                        progressBar1.Value = 0;
+                        label1.Text = "Done!";
+                        //MessageBox.Show("OK");
+
+                    }
+                }
+            }
+        }
+    }
+}
